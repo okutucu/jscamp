0aabfd3e0df6e856b6c3323ff37bc3b2ab7a93cd
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2021-10-21 19:05:36 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Merhaba Kodlama.io");
+
+// JS TYPE SAFE DEĞİLDİR !!
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+
+console.log(dolarBugun)
+
+// Array 
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"];
+
+console.log("<ul>");
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>");
+
+}
+console.log("</ul>");
+
+
+/* <ul>
+    <li>Konut kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */
+
+
+console.log(konutKredileri)
